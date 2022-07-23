3e8d0ef73b46bcbf31e20d103e16a21410788554
tanumaurya <mauryat512@gmail.com>
2022-07-23 20:31:59 +0530

    added css for menu bar

--- Browser/excel/important_points.md
+++ Browser/excel/important_points.md
@@ -1,6 +1,9 @@
 #points
-*height of grid
-* create of cell using js
-* how to create anything dynamically 
-* overflow 
+* height of grid
+   * create of cell using js
+   * how to create anything dynamically 
+   * overflow 
 * positioning
+* Address bar creation
+* menu bar create
+* How to make a drop down
--- Browser/excel/init.js
+++ Browser/excel/init.js
@@ -1,3 +1,4 @@
+// create grid
 function init(){
     let topRow = document.querySelector(".top_row");
     let leftCol = document.querySelector(".left_col");
@@ -40,24 +41,3 @@ function init(){
     }
 }
 init();
-
-
-// address bar implementation
-
-let cells = document.querySelectorAll(".grid .cell");
-let addressBar = document.querySelector(".address_bar");
-// 1. cell eventlistener -> on click
-for(let i=0; i< cells.length; i++){
-    // 2. when a cell is clicked -> element
-    cells[i].addEventListener("click", function (e){
-        let cCell =e.currentTarget;
-        console.log(cCell);
-        // 3.get rid, cid -> address me convert
-        let rid = Number(cCell.getAttribute("rid"));
-        let cid = Number(cCell.getAttribute("cid"));
-        let address = String.fromCharCode(cid + 65) + (rid + 1);
-        console.log(address);
-        //  4.put it into address bar
-        addressBar.value = address;
-    })
-}
